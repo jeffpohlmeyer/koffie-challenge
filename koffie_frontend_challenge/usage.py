e8ea40fe3e372bef8d8872d3c4313ca6fef06537
Jeffrey Pohlmeyer <jeffvp@protonmail.ch>
2022-03-24 01:04:09 -0400

    trying to deploy

--- koffie_frontend_challenge/usage.py
+++ koffie_frontend_challenge/usage.py
@@ -122,271 +122,264 @@ def create_bar_chart(df: pd.DataFrame, chart_type: str) -> Figure:
     return px.bar(df[chart_type].value_counts().sort_index())
 
 
-def main():
-    # Initialize params, fetch data, instantiate app
-    PAGE_SIZE = 20
-    df = fetch_data()
-    size = df.size
-    clicks = 0
-    columns = [{"name": format_name(i), "id": i} for i in df.columns]
+# Initialize params, fetch data, instantiate app
+PAGE_SIZE = 20
+df = fetch_data()
+size = df.size
+clicks = 0
+columns = [{"name": format_name(i), "id": i} for i in df.columns]
 
-    app = Dash(__name__)
+app = Dash(__name__)
 
-    server = app.server
+server = app.server
+
+"""
+Since I've never worked with Plotly and Dash I wasn't sure just
+how much of the markup generation should have been abstracted 
+out so I just left it all in here.  The main structure of the 
+page is as follows:
+<body>
+  <div id="main-header">
+    <div>
+      <h1 id="main-title"></h1>
+      <h2 class="sub-title"></h2>
+      <h2 class="sub-title"></h2>
+    </div>
+    <div id="table-search">
+      <div id="text-search">
+        <label for="column-input"></label>
+        <input id="column-input" />
+      </div>
+      <div>
+        <label for="column-dropdown"></label>
+        <select id="column-dropdown></select>
+      </div>
+      <button>Submit</button>
+    </div>
+  </div>
+  <TableComponent />        <-- This is the React component included
+  <div id="map">
+    <div id="map-container">
+      <h3 id="map-title"></h3>
+      <div id="map-figure"></div>
+    </div>
+  </div>
+  <div id="bar-charts">
+    <div class="jvp-chart">
+      <h3></h3>
+      <div id="year-chart"></div>
+    </div>
+    <div class="jvp-chart">
+      <h3></h3>
+      <div id="make-bar-chart"></div>
+    </div>
+    <div class="jvp-chart">
+      <h3></h3>
+      <div id="static-map-figure"></div>
+    </div>
+  </div>
+</body>
+"""
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            id="main-header",
+            children=[
+                html.Div(
+                    children=[
+                        html.H1(
+                            id="main-title",
+                            children=[
+                                html.Span(
+                                    "Interactive Table of ",
+                                    className="main-title-element",
+                                ),
+                                html.Code(
+                                    "sample.parquet", className="main-title-element"
+                                ),
+                                html.Span(" dataset.", className="main-title-element"),
+                            ],
+                        ),
+                        html.H2("Click on any column to sort", className="sub-title"),
+                        html.H2(
+                            "Click on any row to display the location in the map below the table",
+                            className="sub-title",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    id="table-search",
+                    children=[
+                        html.Div(
+                            id="text-search",
+                            children=[
+                                html.Label("Search Text", htmlFor="column-input"),
+                                dcc.Input(
+                                    id="column-input",
+                                    type="text",
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            children=[
+                                html.Label("Columns", htmlFor="column-dropdown"),
+                                dcc.Dropdown(
+                                    list(map(lambda x: x.get("name"), columns)),
+                                    columns[0].get("name"),
+                                    id="column-dropdown",
+                                ),
+                            ]
+                        ),
+                        html.Button("Submit", id="submit-button", n_clicks=0),
+                    ],
+                ),
+            ],
+        ),
+        koffie_frontend_challenge.TableComponent(
+            data=df.to_dict("records"),
+            id="datatable",
+            columns=columns,
+            page_current=0,
+            page_size=PAGE_SIZE,
+            total_results=size,
+            ascending=0,
+        ),
+        html.Hr(className="divider"),
+        html.Div(
+            id="map",
+            children=[
+                html.Div(
+                    id="map-container",
+                    children=[
+                        html.H3(id="map-title"),
+                        dcc.Graph(id="map-figure", figure={}),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            id="bar-charts",
+            children=[
+                html.Div(
+                    className="jvp-chart",
+                    children=[
+                        html.H3("Vehicle Count by Model Year"),
+                        dcc.Graph(
+                            id="year-chart",
+                            figure=create_bar_chart(df, "MODEL_YEAR"),
+                        ),
+                    ],
+                ),
+                html.Div(
+                    className="jvp-chart",
+                    children=[
+                        html.H3("Vehicle Count by Make"),
+                        dcc.Graph(
+                            id="make-bar-chart",
+                            figure=create_bar_chart(df, "MAKE"),
+                        ),
+                    ],
+                ),
+                html.Div(
+                    className="jvp-chart",
+                    children=[
+                        html.H3("Map of reports per US state"),
+                        dcc.Graph(id="static-map-figure", figure=create_static_map(df)),
+                    ],
+                ),
+            ],
+        ),
+    ]
+)
+
+
+@app.callback(
+    [Output("map-title", "children"), Output("map-figure", "figure")],
+    Input("datatable", "location"),
+    State("datatable", "report_number"),
+)
+def update_dash_map(location, report_number):
+    """
+    This is the code that just updates the map with the passed in location.
+    It geocodes it and tries to update the map with the returned lat and lon
+
+    :param location: A <city>, <state> style string
+    :param report_number: The report number associated with the clicked row
+    :return: Tuple[str, Figure] a string to indicate what was found and an
+        updated figure to refresh the necessary location on the page.
+    """
+    if not location:
+        return "Click the Table to Display a Location", create_variable_map()
+    geocoded = geocode(location)
+
+    # This is what constitutes "error handling" for a location not found.
+    if not geocoded:
+        return f"Location titled {location} was not found", create_variable_map()
+    fig = create_variable_map(geocoded.latitude, geocoded.longitude)
+    return f"Report Number {report_number}: {location}", fig
+
+
+@app.callback(
+    [Output("datatable", "data"), Output("datatable", "page_current")],
+    [
+        Input("datatable", "page_current"),
+        Input("datatable", "column"),
+        Input("datatable", "ascending"),
+        Input("submit-button", "n_clicks"),
+        State("column-dropdown", "value"),
+        State("column-input", "value"),
+    ],
+)
+def update_table(page_current, column, ascending, n_clicks, dropdown, input_val):
+    """
+    This method handles all dataframe manipulation:
+    - Filtering by a text value in a given column
+    - Sorting a single column ascending or descending
+    - Pagination
+
+    :param page_current: The current page of the table
+    :param column: The (optional) column that is being sorted
+    :param ascending: (Optional) whether the sort is ascending (1), descending (-1), or none(0)
+    :param n_clicks: I think this is just needed to listen to button clicks
+    :param dropdown: The value of the #column-dropdown select element
+    :param input_val: The value for which the user is searching in the aforementioned column
+    :return: An updated dataframe and the current page
+    """
+
+    data = df.copy()
+    """
+    Filtering
+    """
+    # Check to see if the event that triggered the callback was a click on the submit button
+    if clicks != n_clicks:
+        if input_val is not None:
+            # If we have an input value then get the ID of the selected column
+            _col = [x.get("id") for x in columns if x.get("name") == dropdown]
+            if _col[0]:
+                # If the column exists then do a case-insensitive str.contains on the column
+                # in the dataframe
+                data = data[data[_col[0]].str.contains(input_val, case=False)]
 
     """
-    Since I've never worked with Plotly and Dash I wasn't sure just
-    how much of the markup generation should have been abstracted 
-    out so I just left it all in here.  The main structure of the 
-    page is as follows:
-    <body>
-      <div id="main-header">
-        <div>
-          <h1 id="main-title"></h1>
-          <h2 class="sub-title"></h2>
-          <h2 class="sub-title"></h2>
-        </div>
-        <div id="table-search">
-          <div id="text-search">
-            <label for="column-input"></label>
-            <input id="column-input" />
-          </div>
-          <div>
-            <label for="column-dropdown"></label>
-            <select id="column-dropdown></select>
-          </div>
-          <button>Submit</button>
-        </div>
-      </div>
-      <TableComponent />        <-- This is the React component included
-      <div id="map">
-        <div id="map-container">
-          <h3 id="map-title"></h3>
-          <div id="map-figure"></div>
-        </div>
-      </div>
-      <div id="bar-charts">
-        <div class="jvp-chart">
-          <h3></h3>
-          <div id="year-chart"></div>
-        </div>
-        <div class="jvp-chart">
-          <h3></h3>
-          <div id="make-bar-chart"></div>
-        </div>
-        <div class="jvp-chart">
-          <h3></h3>
-          <div id="static-map-figure"></div>
-        </div>
-      </div>
-    </body>
+    Sorting
     """
+    if ascending == -1 and column is not None:
+        data.sort_values(column, ascending=False, inplace=True)
+    if ascending == 1 and column is not None:
+        data.sort_values(column, inplace=True)
 
-    app.layout = html.Div(
-        children=[
-            html.Div(
-                id="main-header",
-                children=[
-                    html.Div(
-                        children=[
-                            html.H1(
-                                id="main-title",
-                                children=[
-                                    html.Span(
-                                        "Interactive Table of ",
-                                        className="main-title-element",
-                                    ),
-                                    html.Code(
-                                        "sample.parquet", className="main-title-element"
-                                    ),
-                                    html.Span(
-                                        " dataset.", className="main-title-element"
-                                    ),
-                                ],
-                            ),
-                            html.H2(
-                                "Click on any column to sort", className="sub-title"
-                            ),
-                            html.H2(
-                                "Click on any row to display the location in the map below the table",
-                                className="sub-title",
-                            ),
-                        ]
-                    ),
-                    html.Div(
-                        id="table-search",
-                        children=[
-                            html.Div(
-                                id="text-search",
-                                children=[
-                                    html.Label("Search Text", htmlFor="column-input"),
-                                    dcc.Input(
-                                        id="column-input",
-                                        type="text",
-                                    ),
-                                ],
-                            ),
-                            html.Div(
-                                children=[
-                                    html.Label("Columns", htmlFor="column-dropdown"),
-                                    dcc.Dropdown(
-                                        list(map(lambda x: x.get("name"), columns)),
-                                        columns[0].get("name"),
-                                        id="column-dropdown",
-                                    ),
-                                ]
-                            ),
-                            html.Button("Submit", id="submit-button", n_clicks=0),
-                        ],
-                    ),
-                ],
-            ),
-            koffie_frontend_challenge.TableComponent(
-                data=df.to_dict("records"),
-                id="datatable",
-                columns=columns,
-                page_current=0,
-                page_size=PAGE_SIZE,
-                total_results=size,
-                ascending=0,
-            ),
-            html.Hr(className="divider"),
-            html.Div(
-                id="map",
-                children=[
-                    html.Div(
-                        id="map-container",
-                        children=[
-                            html.H3(id="map-title"),
-                            dcc.Graph(id="map-figure", figure={}),
-                        ],
-                    )
-                ],
-            ),
-            html.Div(
-                id="bar-charts",
-                children=[
-                    html.Div(
-                        className="jvp-chart",
-                        children=[
-                            html.H3("Vehicle Count by Model Year"),
-                            dcc.Graph(
-                                id="year-chart",
-                                figure=create_bar_chart(df, "MODEL_YEAR"),
-                            ),
-                        ],
-                    ),
-                    html.Div(
-                        className="jvp-chart",
-                        children=[
-                            html.H3("Vehicle Count by Make"),
-                            dcc.Graph(
-                                id="make-bar-chart",
-                                figure=create_bar_chart(df, "MAKE"),
-                            ),
-                        ],
-                    ),
-                    html.Div(
-                        className="jvp-chart",
-                        children=[
-                            html.H3("Map of reports per US state"),
-                            dcc.Graph(
-                                id="static-map-figure", figure=create_static_map(df)
-                            ),
-                        ],
-                    ),
-                ],
-            ),
-        ]
+    """
+    Pagination
+    """
+    # Whatever the current page, return only a PAGE_SIZE sized slice of the dataframe
+    return (
+        data.iloc[page_current * PAGE_SIZE : (page_current + 1) * PAGE_SIZE].to_dict(
+            "records"
+        ),
+        page_current,
     )
-
-    @app.callback(
-        [Output("map-title", "children"), Output("map-figure", "figure")],
-        Input("datatable", "location"),
-        State("datatable", "report_number"),
-    )
-    def update_dash_map(location, report_number):
-        """
-        This is the code that just updates the map with the passed in location.
-        It geocodes it and tries to update the map with the returned lat and lon
-
-        :param location: A <city>, <state> style string
-        :param report_number: The report number associated with the clicked row
-        :return: Tuple[str, Figure] a string to indicate what was found and an
-            updated figure to refresh the necessary location on the page.
-        """
-        if not location:
-            return "Click the Table to Display a Location", create_variable_map()
-        geocoded = geocode(location)
-
-        # This is what constitutes "error handling" for a location not found.
-        if not geocoded:
-            return f"Location titled {location} was not found", create_variable_map()
-        fig = create_variable_map(geocoded.latitude, geocoded.longitude)
-        return f"Report Number {report_number}: {location}", fig
-
-    @app.callback(
-        [Output("datatable", "data"), Output("datatable", "page_current")],
-        [
-            Input("datatable", "page_current"),
-            Input("datatable", "column"),
-            Input("datatable", "ascending"),
-            Input("submit-button", "n_clicks"),
-            State("column-dropdown", "value"),
-            State("column-input", "value"),
-        ],
-    )
-    def update_table(page_current, column, ascending, n_clicks, dropdown, input_val):
-        """
-        This method handles all dataframe manipulation:
-        - Filtering by a text value in a given column
-        - Sorting a single column ascending or descending
-        - Pagination
-
-        :param page_current: The current page of the table
-        :param column: The (optional) column that is being sorted
-        :param ascending: (Optional) whether the sort is ascending (1), descending (-1), or none(0)
-        :param n_clicks: I think this is just needed to listen to button clicks
-        :param dropdown: The value of the #column-dropdown select element
-        :param input_val: The value for which the user is searching in the aforementioned column
-        :return: An updated dataframe and the current page
-        """
-
-        data = df.copy()
-        """
-        Filtering
-        """
-        # Check to see if the event that triggered the callback was a click on the submit button
-        if clicks != n_clicks:
-            if input_val is not None:
-                # If we have an input value then get the ID of the selected column
-                _col = [x.get("id") for x in columns if x.get("name") == dropdown]
-                if _col[0]:
-                    # If the column exists then do a case-insensitive str.contains on the column
-                    # in the dataframe
-                    data = data[data[_col[0]].str.contains(input_val, case=False)]
-
-        """
-        Sorting
-        """
-        if ascending == -1 and column is not None:
-            data.sort_values(column, ascending=False, inplace=True)
-        if ascending == 1 and column is not None:
-            data.sort_values(column, inplace=True)
-
-        """
-        Pagination
-        """
-        # Whatever the current page, return only a PAGE_SIZE sized slice of the dataframe
-        return (
-            data.iloc[
-                page_current * PAGE_SIZE : (page_current + 1) * PAGE_SIZE
-            ].to_dict("records"),
-            page_current,
-        )
-
-    app.run_server(debug=True)
 
 
 if __name__ == "__main__":
-    main()
+    app.run_server(debug=True)
